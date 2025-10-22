fe4fd5349ebf02242f5f08f1d51a2a3b9cb3e8a4
6zeropk <nemm3623@naver.com>
2025-10-22 22:11:14 +0900

    feat: MemberMissionRepository 클래스 추가 및 유저 미션 조회 기능 추가

---
+++ src/main/java/com/example/umcchapter5mission/membermission/repository/MemberMissionRepository.java
@@ -0,0 +1,24 @@
+package com.example.umcchapter5mission.membermission.repository;
+
+
+import com.example.umcchapter4mission.memberMission.domain.MemberMission;
+import com.example.umcchapter4mission.memberMission.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+
+    // 해당 유저 모든 미션 보기
+    @Query("select mm from MemberMission mm where mm.member.id = :memberId ")
+    List<MemberMission> findByMemberId(@Param("memberId") long memberId);
+
+    // 해당 유저 특정 상태 지정 미션 보기
+    @Query("select mm from MemberMission mm where mm.member.id = :memberId and mm.status = :status ")
+    List<MemberMission> findByMemberId(@Param("memberId") long memberId, @Param("status") Status status);
+
+}
